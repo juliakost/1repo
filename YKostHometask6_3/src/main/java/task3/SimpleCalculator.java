4bcb12d9f1ddcfb884cf5816173b3eed027a2610
Yuliia Kostitsyna <juliakost2008@gmail.com>
2018-05-03 00:26:46 +0200

    Commit hometask #6_3

---
+++ YKostHometask6_3/src/main/java/task3/SimpleCalculator.java
@@ -0,0 +1,21 @@
+package task3;
+
+public class SimpleCalculator {
+    public double add(double a, double b) {
+        return (a + b);
+    }
+
+    public double subtract(double a, double b) {
+        return (a - b);
+    }
+
+    public double multiplication (double a, double b) {
+        return (a * b);
+    }
+
+    public double divide(double a, double b) {
+        return (a / b);
+    }
+
+
+}
